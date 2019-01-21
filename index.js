bd57713945ace4f19b589dd6e4f6c9572f69dac0
PtitMister84 <46003570+PtitMister84@users.noreply.github.com>
2019-01-21 19:16:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json')
-const token process.env.token. 
+const token process.env.token;
 const prefix = ("?");
-    
+bot.on('ready', fonction () ( 
+    console.log('c'est partit') 
 
 bot.login( token ) ;
